a21e707da04d1f268c87837b7cd6e499ffcccf35
clou <clou@cloudeMacBook-Pro.local>
2016-05-19 17:18:47 +0800

    增加解释说明

--- BatteryAnimation/BatteryAnimation/BatteryChargeView.m
+++ BatteryAnimation/BatteryAnimation/BatteryChargeView.m
@@ -40,6 +40,7 @@
         currentLength = 40;
     }
     _batteryChargeColor = [UIColor colorWithRed:0.0/255 green:green blue:0.0/255 alpha:1.0];
+    //用于调用drawRect：方法，NSTimer每调用一次，执行该方法重绘一次，频率50的时候，就形成了在动的感觉
     [self setNeedsDisplay];
 }
 
@@ -47,7 +48,7 @@
     
     CGContextRef content  = UIGraphicsGetCurrentContext();
     
-    
+    //电池的柱体部分
     CGMutablePathRef path1 = CGPathCreateMutable();
     CGContextSetLineWidth(content, 4);
     CGContextSetFillColorWithColor(content, [[UIColor colorWithRed:84.0/255 green:92.0/255 blue:120.0/255 alpha:1.0] CGColor]);
@@ -63,6 +64,7 @@
     CGContextDrawPath(content, kCGPathStroke);
     CGPathRelease(path1);
     
+    //电池的突出部分
     CGMutablePathRef path2 = CGPathCreateMutable();
     CGContextSetLineWidth(content, 4);
     CGContextSetFillColorWithColor(content, [[UIColor colorWithRed:84.0/255 green:92.0/255 blue:120.0/255 alpha:1.0] CGColor]);
@@ -78,11 +80,11 @@
     CGContextDrawPath(content, kCGPathStroke);
     CGPathRelease(path2);
     
-    
+    //绿色的电量动画部分
     CGMutablePathRef path = CGPathCreateMutable();
-    
     CGContextSetLineWidth(content, 1);
     CGContextSetFillColorWithColor(content, [_batteryChargeColor CGColor]);
+    //电量动画的Max_x部分随时间的变化而变化，最小值为40
     float x = 40+currentLength;
     CGPathMoveToPoint(path,NULL, 84, 84);
     CGPathAddLineToPoint(path, nil, x, 84);
