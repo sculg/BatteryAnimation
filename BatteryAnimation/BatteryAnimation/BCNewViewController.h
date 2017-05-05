75a8d51706d6d24bcfb425305f59588ffdde596a
lisong <lisong7@xiaoniu66.com>
2017-05-05 22:43:31 +0800

    增加一个文件

---
+++ BatteryAnimation/BatteryAnimation/BCNewViewController.h
@@ -0,0 +1,13 @@
+//
+//  BCNewViewController.h
+//  BatteryAnimation
+//
+//  Created by lg on 2017/5/5.
+//  Copyright © 2017年 sculg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BCNewViewController : UIViewController
+
+@end
